801c77c5f94bfc487cc08840636a64a1160f1e30
Neyumi <70411482+Neyumi@users.noreply.github.com>
2024-01-23 15:32:37 +0100

    ai

--- src/game/ai/SmartStrategy.java
+++ src/game/ai/SmartStrategy.java
@@ -4,6 +4,7 @@ import game.model.Board;
 import game.model.Game;
 import game.model.Mark;
 import game.model.Move;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -42,49 +43,41 @@ public class SmartStrategy implements Strategy{
         Random rand = new Random();
         int index = 0;
         List<Move> possibleMoves = game.getValidMoves();
-        int pos = possibleMoves.size();
         int moves = 0;
-        if(moves > Board.DIM*3){ // the ai does random moves until it doesn't matter
-            if(Board.DIM*Board.DIM - moves % 2 == 0){ //it checks how many turns are left from the game
-                while(board.hasSquare(index)) {
+        List<Move> hasSq = new ArrayList<>();
+        List<Move> noSq = new ArrayList<>();
+        for (int j = 0; j< possibleMoves.size();j++){
+            if(board1.hasSquare(j)){
+                hasSq.add(possibleMoves.get(j));
+            } else {
+                noSq.add(possibleMoves.get(j));
+            }
+        }
+        if(noSq.isEmpty()){ // the ai does random moves until it doesn't matter
+
+            if(!(Board.DIM*Board.DIM - moves % 2 == 0)){ //it checks how many turns are left from the game
+                while(!hasSq.isEmpty()) {
                     index = rand.nextInt(possibleMoves.size());
-                    Move thisMove = possibleMoves.get(index);
-                    pos--; // it checks if there are any possible moves that are beneficial
-                    if (!board.hasSquare(index)) {
-                        moves++;
-                        return thisMove;
-                    }
-                    else if(pos == 0){ //if there are no beneficial moves it returns a random move
-                        return thisMove;
-                    }
+                    Move thisMove = hasSq.get(index);
+                    moves++;
+                    return thisMove;
                 }
             }
             else { // if the game has an odd number of moves it will fill in a sqaure to make advantage
-                while (!board.hasSquare(index)) {
-                    index = rand.nextInt(possibleMoves.size());
-                    Move thisMove = possibleMoves.get(index);
-                    pos--;
-                    if (board.hasSquare(index)) {
-                        moves++;
-                        return thisMove;
-                    }else if(pos == 0){
-                        return thisMove;
-                    }
-                }
+                index = rand.nextInt(noSq.size());
+                Move thisMove = noSq.get(index);
+                moves++;
+                return thisMove;
+
             }
 
         }
         //until the it matters the game does random moves
-        while(board.hasSquare(index)){
+        while(!board.hasSquare(index)){
             index = rand.nextInt(possibleMoves.size());
             Move randomMove = possibleMoves.get(index);
-            if(!board.hasSquare(index)) {
-                moves++;
-                pos--;
-                return randomMove;
-            }else if(pos == 0){
-                return randomMove;
-            }
+            moves++;
+            return randomMove;
         }
         return null;
     }
